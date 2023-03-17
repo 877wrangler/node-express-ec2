f1dbf2e4ee28b59360a7aad357d23c4b8e77abab
877wrangler <877wrangler@gmail.com>
2023-03-16 21:10:23 -0400

    Added stock endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import puppeteer from 'puppeteer';
 
 const app = express();
 
@@ -15,4 +16,29 @@ app.get('/', (req, res) => {
     message: 'My API running'
   };
   res.json(data);
+});
+
+// Define the route for option quotes
+app.get('/:symbol', async (req, res) => {
+  const { symbol } = req.params;
+
+  // Launch a new browser instance
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate to the option quote page for the specified symbol
+  await page.goto(`https://finance.yahoo.com/quote/${symbol}`);
+
+  // Wait for the fin-streamer element to be available
+  await page.waitForSelector('fin-streamer');
+
+  // Extract the option quote and type from the page
+  const optionQuote = await page.$$eval('fin-streamer', elements => elements[18].innerHTML);
+  const optionType = await page.$$eval('h1', elements => elements[0].innerHTML);
+
+  // Close the browser instance
+  await browser.close();
+
+  // Return the option quote and type as JSON
+  res.json({ symbol, optionQuote, optionType });
 });
