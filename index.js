da1f498cddb1826203a9044c34fd3ef373e2bb4f
877wrangler <877wrangler@gmail.com>
2023-03-16 21:02:28 -0400

    Added CORS

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+// Allow requests from all domains
+app.use(cors());
+
+// Listen for requests
 app.listen(5001, () => console.log('API running on port 5001'));
 
+// Handle requests to the root URL
 app.get('/', (req, res) => {
   const data = {
     message: 'My API running'
