ec7544dea4dab9f224ac355c9249621b297c066e
877wrangler <877wrangler@gmail.com>
2023-03-16 20:53:42 -0400

    Added valid JSON

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.listen(5001, () => console.log('Api running on port 5001'))
+app.listen(5001, () => console.log('API running on port 5001'));
 
-app.get('/', (req, res) => res.json('My API running'))
+app.get('/', (req, res) => {
+  const data = {
+    message: 'My API running'
+  };
+  res.json(data);
+});
